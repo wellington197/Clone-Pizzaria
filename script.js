db0a9e9da11e131e75bd06d6dd354006a9ed8a68
wellington197 <fcowellcosta@gmail.com>
2022-03-15 15:36:52 -0300

    Fixing the error of the image not showing up.

--- script.js
+++ script.js
@@ -1,10 +1,13 @@
 //Criar um função anônima
 
 const c=(el)=>document.querySelector(el);
+const cs=(el)=>document.querySelectorall(el);
 
+//Função de mapeamento do item
 pizzaJson.map((item, index)=>{
     let pizzaItem = c('.models .pizza-item').cloneNode(true);
     //preencher as informações em pizzaitem
 
+    //Listar as pizzas na área 
     c('.pizza-area').append(pizzaItem);
 });
