b6f2ac77bf176b67ae841dc8b2d7edf579b8435c
wellington197 <fcowellcosta@gmail.com>
2022-03-28 09:52:02 -0300

    Create functions for cart

--- pizzas.js
+++ pizzas.js
@@ -1,3 +1,31 @@
+
+/*
+
+let pizzaJson = [
+    {id:1, name:'Mussarela', img:'images/pizza.png', price:[10.50, 15.60, 20.19], sizes:['100g', '530g', '860g'], description:'Descrição da pizza em mais de uma linha muito legal bem interessante'},
+    {id:2, name:'Calabresa', img:'images/pizza2.png', price:[8.50, 14.70, 18.00], sizes:['320g', '530g', '860g'], description:'Descrição da pizza em mais de uma linha muito legal bem interessante'},
+    {id:3, name:'Quatro Queijos', img:'images/pizza3.png', price:[9.50, 16.40, 27.45], sizes:['320g', '530g', '860g'], description:'Descrição da pizza em mais de uma linha muito legal bem interessante'},
+    {id:4, name:'Americana', img:'images/pizza4.png', price:[12.50, 16.00, 29.77], sizes:['320g', '530g', '860g'], description:'Descrição da pizza em mais de uma linha muito legal bem interessante'},
+    {id:5, name:'Sorvete', img:'images/pizza5.png', price:[10.50, 14.50, 21.43], sizes:['320g', '530g', '860g'], description:'Descrição da pizza em mais de uma linha muito legal bem interessante'},
+    {id:6, name:'Moda da Casa', img:'images/pizza6.png', price:[10.00, 20.00, 30.00], sizes:['320g', '530g', '860g'], description:'Descrição da pizza em mais de uma linha muito legal bem interessante'},
+    {id:7, name:'Chocolate', img:'images/pizza7.png', price:[8.60, 11.50, 22.36], sizes:['320g', '530g', '860g'], description:'Descrição da pizza em mais de uma linha muito legal bem interessante'},
+    {id:8, name:'Marguerita', img:'images/pizza8.png', price:[12.50, 16.00, 29.77], sizes:['320g', '530g', '860g'], description:'Descrição da pizza em mais de uma linha muito legal bem interessante'}
+
+
+];
+
+*/
+
+
+
+
+
+
+
+
+
+
+
 let pizzaJson = [
     {id:1, name:'Mussarela', img:'images/pizza.png', price:20.19, sizes:['100g', '530g', '860g'], description:'Descrição da pizza em mais de uma linha muito legal bem interessante'},
     {id:2, name:'Calabresa', img:'images/pizza2.png', price:18.00, sizes:['320g', '530g', '860g'], description:'Descrição da pizza em mais de uma linha muito legal bem interessante'},
